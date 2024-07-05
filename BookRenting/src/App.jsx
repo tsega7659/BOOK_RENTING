6df4aed7f307575ef8ed4eda931289c58e2e109d
tsega7664 <yeabsira.zelalem@aastustudent.edu.et>
2024-07-05 21:55:24 +0300

    first commit

---
+++ BookRenting/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, World!
+      </h1>
+    </div>
+  )
+}
+
+export default App
